b70315acc9cff10494919be4b777d20fce5cc621
polianarm <polianariveiro@gmail.com>
2023-10-25 19:55:28 -0300

    ajustando o navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -4,18 +4,25 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
+     <div>
+        <ul>
+        <li className={styles.item}>
+            <Link to="/">digital booking</Link>
+          </li>
+        </ul>
+     </div>   
+
       <div className="navbar-login" id="navbar-login">
         <ul className={styles.list}>
-          
-          <li className={styles.item}>
-            <Link to="/">Home</Link>
-          </li>
-
-          <li className={styles.item}>
-            <Link to="/register">Criar conta</Link>
+        <li className={styles.item}>
+            <Link to="/register">
+              <button className={styles.button}>Criar conta</button>
+            </Link>
           </li>
           <li className={styles.item}>
-            <Link to="/login">Iniciar sessão</Link>
+            <Link to="/login">
+              <button className={styles.button}>Iniciar sessão</button>
+            </Link>
           </li>
         </ul>
       </div>
