f205601a528fac9c41e48c90b18e376251b64c62
polianarm <polianariveiro@gmail.com>
2023-10-25 21:14:11 -0300

    adicionando elementos de input no header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,21 @@
-
+import styles from "./Header.module.css";
 
 const Header = () => {
 
     return (
         <>
-        
-        <h1>Alugue um carro</h1>
+        <div className={styles.header}>
+          <div className={styles.searchBar}>
+            <input type="text" placeholder="Local de Retirada" className={styles.searchInput} />
+            <input type="datetime-local" className={styles.dateTimeInput} />
+            <input type="datetime-local" className={styles.dateTimeInput} />
+        <button className={styles.searchButton}>Buscar 🔎</button>
+          </div>
+          <div className={styles.checkboxContainer}>
+           <input type="checkbox" id="devolver-outro-lugar" />
+           <label htmlFor="devolver-outro-lugar">Devolver em outro lugar</label>
+           </div>
+        </div>
         
         </>
 
