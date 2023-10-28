783b889c6390f9c12071d3330b202887962b65b8
andrericao <andrericao@hotmail.com>
2023-10-28 10:24:50 -0300

    validacao do cadastro e insercao do favicon

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,81 +1,93 @@
 import { useState } from 'react';
+// useForm
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+//yup
+import {object, ref, string} from "yup";
+
 import styles from './Register.module.css';
 import eye from '../../assets/eye.svg';
 import eyeslash from '../../assets/eye-slash.svg';
 
 export default function Register(){
 
-	const [form, serForm] = useState({
-		nome: "",
-		sobrenome: "",
-		email: ""		,
-		password: "",
-		confirmPasswor: ""
+	const [showPasswordOne, setShowPasswordOne] = useState(false);
+	const [showPasswordTwo, setShowPasswordTwo] = useState(false);
+
+	const schema = object({
+		
+		nome: string().min(3, "* Mínimo de 3 caracteres!")
+		.required("* Campo Obrigatório!"),
+
+		sobrenome: string()
+		.required("* Mínimo de 3 caracteres!"),
+
+		email: string()
+		.email()
+		.required("* Insira um e-mail válido"),
+
+		password: string()
+		.min(8, "* Mínimo de 8 caracteres")
+		.required("* Campo Obrigatório!"),
+
+		confirmPassword: string()
+		.min(8, "* Mínimo de 8 caracteres")
+		.required("* Repita a senha!")
+		.oneOf([ref("password")], "* Senha estão diferentes!")
 	})
 
-	
-		const [showPasswordOne, setShowPasswordOne] = useState(false);
-		const [showPasswordTwo, setShowPasswordTwo] = useState(false);
+	const { register, 
+		handleSubmit: onSubmit, 
+		watch, 
+		formState: { errors } 
+	} = useForm({resolver: yupResolver(schema)});
 
-		/*
-			const [fraseError, setFraseError] = useState('');
-			const [textoIpunt, setTextoInput] = useState('');
-		*/
+	const handleSubmit = (data) => {
+		console.log(data);
+	}
+
 		const handleClickShowPasswordOne = () => {
 			setShowPasswordOne(!showPasswordOne)
 		}
 
 		const handleClickShowPasswordTwo = () => {
 			setShowPasswordTwo(!showPasswordTwo)
-		}
-
-		/*
-		const handleInputError1 = () => {
-
-			setInputError(!inputError)
-		}
-		
-		const handleInputError = (value) => {
-
-			if(value.length < 3){
-				setFraseError('* Insira no mínimo 3 caracteres!');
-			} else {
-				setFraseError('');
-			}
-			setTextoInput(value);
-		}*/
-		
-		
+		}		
 	
 	return (
 		<>
-			<form action="">
+			<form onSubmit={onSubmit(handleSubmit)}>
 
 				<fieldset> MoveRental
 
 				<div className={styles.user_input}>
 
-					<label htmlFor="">
+					<label>
 						Nome:
 					</label>
 					<div>
 						<input name="nome" 
 						type="text" 
 						minLength={3} 
-						onChange={(event) => handleChange(event.target.value)}/>
-						<small>* Insira no mínimo 3 caracteres!</small>
+						{...register("nome")}
+						//onChange={(event) => handleChange(event.target.value)}
+						/>
+						<small className={styles.error}>{errors?.nome?.message}</small>
 					</div>
 
 					<label htmlFor="">
 						Sobrenome
 					</label>
-					<div>
+					<div className={styles.user_input}>
 						<input name="sobrenome" 
 						type="text" 
-						minLength={3} 
-						onChange={(event) => handleChange(event.target.value)}/>
+						minLength={3}
+						{...register("sobrenome")}
+						//onChange={(event) => handleChange(event.target.value)}
+						/>
 						
-						<small>* Insira no mínimo 3 caracteres</small>
+						<small className={styles.error}>{errors?.sobrenome?.message}</small>
 					</div>
 				</div>
 
@@ -84,8 +96,13 @@ export default function Register(){
 						Email:
 					</label>
 					<div>
-						<input type="email" name="email" id="email" />
-						<small>* Insira um e-mail válido</small>
+						<input 
+						type="email" 
+						name="email"
+						id="email" 
+						{...register("email")}
+						/>
+						<small className={styles.error}>{errors?.email?.message}</small>
 					</div>
 				</div>
 
@@ -94,15 +111,20 @@ export default function Register(){
 						Senha:
 					</label>
 					<div className={styles.divPasswordEConfirmPassword}>
-						<input type={!showPasswordOne ? "password" : "text"} name="password" id="password" minLength={8}/>
+						<input type={!showPasswordOne ? "password" : "text"} name="password" 
+						id="password" 
+						minLength={8}
+						{...register("password")}
+						/>
 
 						<img onClick={handleClickShowPasswordOne} 
 							className={styles.eye_password_bt}
-							id="imgPasswordOne" 
+							id="imgPasswordOne"
 							src={!showPasswordOne ? eye : eyeslash}
-							alt="icone olho" />
+							alt="icone olho" 
+							/>
 					</div>
-					<small>* Sua senha precisa ter no mínimo 8 caracteres</small>
+					<small className={styles.error}>{errors?.password?.message}</small>
 				</div>
 
 				<div className={styles.user_input}>
@@ -110,14 +132,20 @@ export default function Register(){
 						Comfirmar Senha:
 					</label>
 					<div className={styles.divPasswordEConfirmPassword}>
-						<input type={!showPasswordTwo ? "password" : "text"} name="confirmPassword" id="confirmPassword" minLength={8} />
+						<input type={!showPasswordTwo ? "password" : "text"} name="confirmPassword" 
+						id="confirmPassword" 
+						minLength={8} 
+						{...register("confirmPassword")}
+
+						/>
 						<img onClick={handleClickShowPasswordTwo} 
 							className={styles.eye_password_bt}
 							id="imgPasswordTwo" 
 							src={!showPasswordTwo ? eye : eyeslash}
-							alt="icone olho" />
+							alt="icone olho" 
+							/>
 					</div>
-					<small>* As senhas não correspondem</small>
+					<small className={styles.error}>{errors?.confirmPassword?.message}</small>
 					
 
 					<div className={styles.div_button}>
