826174c1e438930aa1c6805ecbd530e0d910f944
polianarm <polianariveiro@gmail.com>
2023-11-09 15:36:02 -0300

    subindo galeria, ainda falta responsividade

--- src/Components/DetailGaleria/DetailGaleria.jsx
+++ src/Components/DetailGaleria/DetailGaleria.jsx
@@ -1,5 +1,6 @@
 import styles from './DetailGaleria.module.css';
 import { HiOutlineHeart, HiOutlineShare  } from "react-icons/hi";
+import { Link } from 'react-router-dom';
 
 
 const DetailGaleria = () => {
@@ -15,20 +16,21 @@ const DetailGaleria = () => {
         <div className={styles.containerBox}>
         
             <div className={styles.box1}>
-                <img className={styles.imgPrincipal} src="" alt="Descrição da Imagem" />
+                <img className={styles.imgCar} src="https://images.pexels.com/photos/93632/pexels-photo-93632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
             </div>
         
             <div className={styles.box2}>
-                <img className={styles.imgSub} src="" alt="Descrição da Imagem" />
+                <img className={styles.imgCar} src="https://images.pexels.com/photos/11790230/pexels-photo-11790230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
             </div>
             <div className={styles.box3}>
-                <img className={styles.imgSub} src="" alt="Descrição da Imagem" />
+                <img className={styles.imgCar} src="https://images.pexels.com/photos/18369294/pexels-photo-18369294/free-photo-of-automotivo-automotor-automobilistico-carro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
             </div>
             <div className={styles.box4}>
-                <img className={styles.imgSub} src="" alt="Descrição da Imagem" />
+                <img className={styles.imgCar} src="https://images.pexels.com/photos/7508527/pexels-photo-7508527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
             </div>
             <div className={styles.box5}>
-                <img className={styles.imgSub} src="" alt="Descrição da Imagem" />
+                <img className={styles.imgCar} src="https://images.pexels.com/photos/11877391/pexels-photo-11877391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
+                <Link to="/" className={styles.link}>Ver mais</Link>
             </div>
         </div>
    </div> 
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -3,7 +3,6 @@ import Footer from "../../Components/Footer/Footer";
 import DetailHeader from "../../Components/DetailHeader/DetailHeader";
 import DetailLocalizacao from "../../Components/DetailLocalizacao/DetaielLocalizacao";
 import DetailGaleria from "../../Components/DetailGaleria/DetailGaleria";
-// import DetailDescricao from "../../Components/DetailDescricao/DetailDescricao";
 import DetailCaracteristica from "../../Components/DetailCaracteristica/DetailCaracteristica";
 
 export function DetailPage() {
@@ -15,7 +14,6 @@ return (
             <DetailHeader/>
             <DetailLocalizacao/>
             <DetailGaleria/>
-            {/* <DetailDescricao/> */}
             <DetailCaracteristica/>
         </main> 
     <Footer /> 
