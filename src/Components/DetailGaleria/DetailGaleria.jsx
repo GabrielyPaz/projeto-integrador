b6affc581de410bb85db6c583b465748bbe73ee8
Andrea <dea0312@gmail.com>
2023-11-14 22:16:20 -0300

    inserindo carrossel mobile e tablet

--- src/Components/DetailGaleria/DetailGaleria.jsx
+++ src/Components/DetailGaleria/DetailGaleria.jsx
@@ -1,46 +1,124 @@
-import styles from './DetailGaleria.module.css';
-import { HiOutlineHeart, HiOutlineShare  } from "react-icons/hi";
-import { Link } from 'react-router-dom';
+import styles from "./DetailGaleria.module.css";
+import { HiOutlineHeart, HiOutlineShare } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.css';
+
+import { register } from "swiper/element/bundle";
+register();
 
 const DetailGaleria = () => {
+  
+  const slides = [
+    {
+      id: 1,
+      imagem:
+        "https://images.pexels.com/photos/93632/pexels-photo-93632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      imagem:
+        "https://images.pexels.com/photos/11790230/pexels-photo-11790230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      imagem:
+        "https://images.pexels.com/photos/18369294/pexels-photo-18369294/free-photo-of-automotivo-automotor-automobilistico-carro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 4,
+      imagem:
+        "https://images.pexels.com/photos/7508527/pexels-photo-7508527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 5,
+      imagem:
+        "https://images.pexels.com/photos/11877391/pexels-photo-11877391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ];
 
-
-   return (
+  return (
     <div className={styles.container}>
-        <div className={styles.actions}>
-            <HiOutlineShare size={25} />
-            <HiOutlineHeart size={25}/>
+      <div className={styles.actions}>
+        <HiOutlineShare size={25} />
+        <HiOutlineHeart size={25} />
+      </div>
 
-        </div>
-        <div className={styles.containerBox}>
-        
-            <div className={styles.box1}>
-                <img className={styles.imgCar} src="https://images.pexels.com/photos/93632/pexels-photo-93632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
-            </div>
-        
-            <div className={styles.box2}>
-                <img className={styles.imgCar} src="https://images.pexels.com/photos/11790230/pexels-photo-11790230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
-            </div>
-            <div className={styles.box3}>
-                <img className={styles.imgCar} src="https://images.pexels.com/photos/18369294/pexels-photo-18369294/free-photo-of-automotivo-automotor-automobilistico-carro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
-            </div>
-            <div className={styles.box4}>
-                <img className={styles.imgCar} src="https://images.pexels.com/photos/7508527/pexels-photo-7508527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
-            </div>
-            <div className={styles.box5}>
-                <img className={styles.imgCar} src="https://images.pexels.com/photos/11877391/pexels-photo-11877391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Descrição da Imagem" />
-                <Link to="/" className={styles.link}>Ver mais</Link>
-            </div>
+      <div className={styles.containerBox}>
+        <div className={styles.box1}>
+          <img
+            className={styles.imgCar}
+            src="https://images.pexels.com/photos/93632/pexels-photo-93632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Descrição da Imagem"
+          />
         </div>
 
-
-
-
-   </div> 
-     
-   )
-}
-
+        <div className={styles.box2}>
+          <img
+            className={styles.imgCar}
+            src="https://images.pexels.com/photos/11790230/pexels-photo-11790230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Descrição da Imagem"
+          />
+        </div>
+        <div className={styles.box3}>
+          <img
+            className={styles.imgCar}
+            src="https://images.pexels.com/photos/18369294/pexels-photo-18369294/free-photo-of-automotivo-automotor-automobilistico-carro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Descrição da Imagem"
+          />
+        </div>
+        <div className={styles.box4}>
+          <img
+            className={styles.imgCar}
+            src="https://images.pexels.com/photos/7508527/pexels-photo-7508527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Descrição da Imagem"
+          />
+        </div>
+        <div className={styles.box5}>
+          <img
+            className={styles.imgCar}
+            src="https://images.pexels.com/photos/11877391/pexels-photo-11877391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Descrição da Imagem"
+          />
+          <Link to="/" className={styles.link}>
+            Ver mais
+          </Link>
+        </div>
+      </div>
+    
+      <div className={styles.carrosselContainer}>       
+          <swiper-container
+            //navigation
+            pagination={{ clickable: true }}
+                      
+            effect="coverflow"
+            grabCursor
+            centeredSlides
+            slidesPerView={1}
+            speed={400}   
+                   
+            className={styles.carrossel}
+          >
+            {slides.map((item) => (
+              <SwiperSlide key={item.id} className={styles.carrosselItem}>
+                <img
+                  src={item.imagem}
+                  className={styles.carrosselImagem}
+                  alt=""
+                />
+                <span className={styles.indexAtual}>
+                  {" "}
+                  {item.id} / {slides.length}{" "}
+                </span>
+              </SwiperSlide>
+            ))}
+          </swiper-container>       
+      </div>
+    </div>
+  );
+};
 
 export default DetailGaleria;
--- src/Components/DetailMapa/DetailMapa.jsx
+++ src/Components/DetailMapa/DetailMapa.jsx
@@ -7,14 +7,12 @@ export default function DetailMapa() {
     return (
       <div className={styles.containerMapa}>
         <h1 className={styles.titleMapa}> Localização </h1>
-
-        {/*<p className={styles.locationTitle}> {} </p>*/}
-
+        
         <div className={styles.mapaArea}>
           <MapContainer className={styles.mapa} center={[-22.9031, -43.255]} zoom={15}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              //attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              
             />
             <Marker position={[-22.9031, -43.255]}>
               <Popup> Sua Localização </Popup>
