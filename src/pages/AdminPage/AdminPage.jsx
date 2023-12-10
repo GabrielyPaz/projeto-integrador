91d3b85f7468c7af944cc42d7d529a22cc303f90
Andrea <dea0312@gmail.com>
2023-12-09 21:27:44 -0300

    inserindo header admin, configurando mensagem de erro registerform

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,15 +1,27 @@
 import Navbar from "../../Components/NavBar/Navbar";
 import Footer from "../../Components/Footer/Footer";
+//import { useNavigate } from 'react-router-dom';
+//import { useEffect, useState } from 'react';
 import AdminFormulario from "../../Components/AdminFormulario/AdminFormulario";
 
 
 export function AdminPage() {
 
+  /*  const navigate = useNavigate();
+    const [isAdmin] = useState();
+   
+    useEffect(() => {
+        if(isAdmin != 'ADMIN') {
+            navigate('/');
+        }
+    }, [isAdmin, navigate]);*/
+
 return (
     <>
         <Navbar />
         <main>
-            <AdminFormulario />
+          { /* { isAdmin != null && ( <AdminFormulario /> ) } */}
+           <AdminFormulario />
         </main>
         <Footer />
     </>
